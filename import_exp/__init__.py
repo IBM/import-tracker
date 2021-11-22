6d8cecb1185afda223c8d55c52641835aec70a72
Gabe Goodhart <ghart@us.ibm.com>
2021-11-22 16:57:20 -0700

    main: Hack hack hack towards a trackable module manager library
    Added LazyModule wrapper and configuration for import modes. The things
    left are:
    
    1. Implement _track_deps to run the proactive importing in a subprocess and
       track the resulting module deltas
    2. Set up the framework for a static file-based version of the tracking
       results that will allow the tracking to be an offline operation

--- import_exp/__init__.py
+++ import_exp/__init__.py
@@ -4,7 +4,7 @@ tracks their third party deps
 """
 
 # First import to set the bar for the baseline standard modules
-from ._import_util import import_tracked_module as _import_tracked_module
+from ._import_util import import_module as _import_tracked_module
 from ._import_util import get_required_imports, get_required_packages
 
 # Use the import tool to import and track the sub modules
--- import_exp/_import_util.py
+++ import_exp/_import_util.py
@@ -13,23 +13,38 @@ import sys
 import warnings
 
 
-## Globals #####################################################################
+## Public Globals ##############################################################
 
-g_module_dep_mapping = {}
+# Possible import modes
+LAZY = "LAZY"
+PROACTIVE = "PROACTIVE"
+TRACKING = "TRACKING"
+
+# The env var used to check the mode
+MODE_ENV_VAR = "IMPORT_TRACKER_MODE"
+
+## Impl Globals ################################################################
+
+# The configured version. This defaults to lazy importing.
+_import_mode = os.environ.get(MODE_ENV_VAR, LAZY)
+
+# The global mapping from modules to dependencies
+_module_dep_mapping = {}
 
 # The path where global modules are found
-g_std_lib_dir = os.path.realpath(os.path.dirname(os.__file__))
+_std_lib_dir = os.path.realpath(os.path.dirname(os.__file__))
 
 # Lazily created global mapping from module name to package name
-g_module_to_pkg = None
+_module_to_pkg = None
 
-# Implementation detail exprs for finding module names
+# Exprs for finding module names
 _pkg_version_expr = re.compile("-[0-9]")
 _pkg_name_expr = re.compile("^Name: ([^\s]+)")
 
+
 ## Utils #######################################################################
 
-def import_tracked_module(module_name: str, *args, **kwargs) -> ModuleType:
+def import_module(module_name: str, *args, **kwargs) -> ModuleType:
     """Import a module by name and keep track of the changes in the global
     modules dict
     """
@@ -37,32 +52,35 @@ def import_tracked_module(module_name: str, *args, **kwargs) -> ModuleType:
     if module_name in sys.modules:
         return sys.modules[module_name]
 
-    # Do the import
-    imported = importlib.import_module(module_name, *args, **kwargs)
+    # If performing a PROACTIVE import, import directly and return
+    if _import_mode == PROACTIVE:
+        return importlib.import_module(module_name, *args, **kwargs)
 
-    # Get the snapshot of non-standard modules after importing
-    global g_module_dep_mapping
-    g_module_dep_mapping[imported.__name__] = _get_non_std_modules()
+    # If we're doing a TRACKING import. In this case, we're going to perform the
+    # the tracking in a subprocess and then return a lazy importer
+    if _import_mode == TRACKING:
+        _track_deps(module_name, *args, **kwargs)
 
-    # Return the imported module
-    return imported
+    # For either LAZY or TRACKING, we return a Lazy Importer
+    return LazyModule(module_name, *args, **kwargs)
 
 
 def get_required_imports(module_name: str) -> Set[str]:
     """Get the set of modules that are required for the given module by name. If
     the module is not known, ImportError is raised
     """
-    if module_name not in g_module_dep_mapping:
+    # TODO: If module not found, look in static module mapping
+    if module_name not in _module_dep_mapping:
         raise ImportError(f"Cannot get required imports for untracked module [{module_name}]")
-    return g_module_dep_mapping[module_name]
+    return _module_dep_mapping[module_name]
 
 
 def get_required_packages(module_name: str) -> Set[str]:
     """Get the set of installable packages required by this names module"""
     # Lazily create the global mapping
-    global g_module_to_pkg
-    if g_module_to_pkg is None:
-        g_module_to_pkg = _map_modules_to_package_names()
+    global _module_to_pkg
+    if _module_to_pkg is None:
+        _module_to_pkg = _map_modules_to_package_names()
 
     # Get all required imports
     required_modules = get_required_imports(module_name)
@@ -70,14 +88,55 @@ def get_required_packages(module_name: str) -> Set[str]:
     # Merge the required packages for each
     required_pkgs = set()
     for mod in required_modules:
-        if mod in g_module_to_pkg:
-            required_pkgs.update(g_module_to_pkg[mod])
+        if mod in _module_to_pkg:
+            required_pkgs.update(_module_to_pkg[mod])
         else:
             warnings.warn(f"Could not find any required packages for {mod}")
     return required_pkgs
 
 
 ## Implementation Details ######################################################
+
+
+class LazyModule(ModuleType):
+    """A LazyModule is a module subclass that wraps another module but imports
+    it lazily and then aliases __getattr__ to the lazily imported module.
+    """
+
+    def __init__(self, module_name: str, *args, **kwargs):
+        """Hang onto the import args to use lazily"""
+        self.__module_name = module_name
+        self.__args = args
+        self.__kwargs = kwargs
+        self.__wrapped_module = None
+
+    def __getattr__(self, name: str) -> any:
+        """When asked for an attribute, make sure the wrapped module is imported
+        and then delegate
+        """
+        if self.__wrapped_module is None:
+            self.__wrapped_module = importlib.import_module(
+                self.__module_name, *self.__args, **self.__kwargs,
+            )
+        return getattr(self.__wrapped_module, name)
+
+
+def _track_deps(module_name: str, *args, **kwargs):
+    """DEBUG!!!!!!!!!!!!!!!
+
+    This needs to be properly implemented
+    """
+
+    # Do the import
+    imported = importlib.import_module(module_name, *args, **kwargs)
+
+    # Get the snapshot of non-standard modules after importing
+    global _module_dep_mapping
+    _module_dep_mapping[imported.__name__] = _get_non_std_modules()
+
+    # Return the imported module
+    return imported
+
 
 def _get_non_std_modules() -> Set[str]:
     """Take a snapshot of the non-standard modules currently imported"""
@@ -87,7 +146,7 @@ def _get_non_std_modules() -> Set[str]:
         for mod_name, mod in sys.modules.items()
         if not mod_name.startswith("_")
         and "." not in mod_name
-        and _get_import_parent_path(mod) != g_std_lib_dir
+        and _get_import_parent_path(mod) != _std_lib_dir
         and os.path.splitext(mod.__file__)[-1] not in [".so", ".dylib"]
         and mod_name.split(".")[0] != this_module
     }
@@ -97,7 +156,7 @@ def _get_import_parent_path(mod) -> str:
     """Get the parent directory of the given module"""
     # Some standard libs have no __file__ attribute
     if not hasattr(mod, "__file__"):
-        return g_std_lib_dir
+        return _std_lib_dir
 
     # If the module comes from an __init__, we need to pop two levels off
     file_path = mod.__file__
