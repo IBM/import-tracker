057cb262c6740eb7a9aa02b475ed40cd38a7f19a
Gabe Goodhart <ghart@us.ibm.com>
2021-11-23 09:56:19 -0700

    main: Sample lib __init__ cleanup

--- sample_lib/__init__.py
+++ sample_lib/__init__.py
@@ -2,10 +2,8 @@
 Sample library to test out the import_tracker functionality
 """
 
-from import_tracker import import_module
+from import_tracker import import_module as _import_module
 
 # Use the import tool to import and track the sub modules
-submod1 = import_module(".submod1", __name__)
-submod2 = import_module(".submod2", __name__)
-
-__all__ = ["submod1", "submod2"]
+submod1 = _import_module(".submod1", __name__)
+submod2 = _import_module(".submod2", __name__)
