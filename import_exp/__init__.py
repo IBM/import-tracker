cec2b5aed8a53dec37016fc9cce394848baad184
Gabe Goodhart <ghart@us.ibm.com>
2021-11-23 09:44:40 -0700

    main: Fully functional implementation of tracking mode

--- import_exp/__init__.py
+++ import_exp/__init__.py
@@ -4,9 +4,8 @@ tracks their third party deps
 """
 
 # First import to set the bar for the baseline standard modules
-from ._import_util import import_module as _import_tracked_module
-from ._import_util import get_required_imports, get_required_packages
+from ._import_util import get_required_imports, get_required_packages, import_module
 
 # Use the import tool to import and track the sub modules
-submod1 = _import_tracked_module(".submod1", __name__)
-submod2 = _import_tracked_module(".submod2", __name__)
+submod1 = import_module(".submod1", __name__)
+submod2 = import_module(".submod2", __name__)
--- import_exp/_import_util.py
+++ import_exp/_import_util.py
@@ -5,10 +5,13 @@ through import statements
 
 # Standard
 from types import ModuleType
-from typing import Dict, Set
+from typing import Dict, List, Optional
 import importlib
+import json
 import os
 import re
+import shlex
+import subprocess
 import sys
 import warnings
 
@@ -41,38 +44,38 @@ _pkg_name_expr = re.compile("^Name: ([^\s]+)")
 
 ## Utils #######################################################################
 
-def import_module(module_name: str, *args, **kwargs) -> ModuleType:
+def import_module(name: str, package: Optional[str] = None) -> ModuleType:
     """Import a module by name and keep track of the changes in the global
     modules dict
     """
     # If this module is already imported, just return it
-    if module_name in sys.modules:
-        return sys.modules[module_name]
+    if name in sys.modules:
+        return sys.modules[name]
 
     # If performing a PROACTIVE import, import directly and return
     if _import_mode == PROACTIVE:
-        return importlib.import_module(module_name, *args, **kwargs)
+        return importlib.import_module(name, package)
 
     # If we're doing a TRACKING import. In this case, we're going to perform the
     # the tracking in a subprocess and then return a lazy importer
     if _import_mode == TRACKING:
-        _track_deps(module_name, *args, **kwargs)
+        _track_deps(name, package)
 
     # For either LAZY or TRACKING, we return a Lazy Importer
-    return LazyModule(module_name, *args, **kwargs)
+    return LazyModule(name, package)
 
 
-def get_required_imports(module_name: str) -> Set[str]:
+def get_required_imports(name: str) -> List[str]:
     """Get the set of modules that are required for the given module by name. If
     the module is not known, ImportError is raised
     """
     # TODO: If module not found, look in static module mapping
-    if module_name not in _module_dep_mapping:
-        raise ImportError(f"Cannot get required imports for untracked module [{module_name}]")
-    return _module_dep_mapping[module_name]
+    if name not in _module_dep_mapping:
+        raise ImportError(f"Cannot get required imports for untracked module [{name}]")
+    return _module_dep_mapping[name]
 
 
-def get_required_packages(module_name: str) -> Set[str]:
+def get_required_packages(name: str) -> List[str]:
     """Get the set of installable packages required by this names module"""
     # Lazily create the global mapping
     global _module_to_pkg
@@ -80,7 +83,7 @@ def get_required_packages(module_name: str) -> Set[str]:
         _module_to_pkg = _map_modules_to_package_names()
 
     # Get all required imports
-    required_modules = get_required_imports(module_name)
+    required_modules = get_required_imports(name)
 
     # Merge the required packages for each
     required_pkgs = set()
@@ -89,7 +92,7 @@ def get_required_packages(module_name: str) -> Set[str]:
             required_pkgs.update(_module_to_pkg[mod])
         else:
             warnings.warn(f"Could not find any required packages for {mod}")
-    return required_pkgs
+    return sorted(list(required_pkgs))
 
 
 ## Implementation Details ######################################################
@@ -100,11 +103,10 @@ class LazyModule(ModuleType):
     it lazily and then aliases __getattr__ to the lazily imported module.
     """
 
-    def __init__(self, module_name: str, *args, **kwargs):
+    def __init__(self, name: str, package: Optional[str] = None):
         """Hang onto the import args to use lazily"""
-        self.__module_name = module_name
-        self.__args = args
-        self.__kwargs = kwargs
+        self.__name = name
+        self.__package = package
         self.__wrapped_module = None
 
     def __getattr__(self, name: str) -> any:
@@ -113,26 +115,35 @@ class LazyModule(ModuleType):
         """
         if self.__wrapped_module is None:
             self.__wrapped_module = importlib.import_module(
-                self.__module_name, *self.__args, **self.__kwargs,
+                self.__name, self.__package,
             )
         return getattr(self.__wrapped_module, name)
 
 
-def _track_deps(module_name: str, *args, **kwargs):
-    """DEBUG!!!!!!!!!!!!!!!
-
-    This needs to be properly implemented
+def _track_deps(name: str, package: Optional[str] = None):
+    """This function is used to track the dependencies needed for an individual
+    module. It does this by launching a subprocess to import that dependency in
+    isolation and merges the reported results into the global mapping.
     """
+    # Run this package as a subprocess and collect the results
+    cmd = "{} -m {} --name {}".format(
+        sys.executable,
+        sys.modules[__name__].__package__,
+        name,
+    )
+    if package is not None:
+        cmd += f" --package {package}"
+    res = subprocess.run(
+        shlex.split(cmd),
+        stdout=subprocess.PIPE,
+        env={MODE_ENV_VAR: LAZY},
+    )
+    assert res.returncode == 0, f"Failed to track {name}"
+    deps = json.loads(res.stdout)
 
-    # Do the import
-    imported = importlib.import_module(module_name, *args, **kwargs)
-
-    # Get the snapshot of non-standard modules after importing
+    # Merge the results into the global mapping
     global _module_dep_mapping
-    _module_dep_mapping[imported.__name__] = _get_non_std_modules()
-
-    # Return the imported module
-    return imported
+    _module_dep_mapping.update(deps)
 
 
 def _map_modules_to_package_names():
