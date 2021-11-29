cbb56405599fe54197d411e0fe1272a10f95692e
Gabe Goodhart <ghart@us.ibm.com>
2021-11-29 16:28:24 -0700

    main: get rid of src/ indirection

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,9 +5,9 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$BASE_DIR"
 
 FAIL_THRESH=40.0
-PYTHONPATH="${BASE_DIR}/src" python3 -m pytest \
+python3 -m pytest \
     --cov-config=.coveragerc \
-    --cov=src \
+    --cov=import_tracker \
     --cov-report=term \
     --cov-report=html \
     --cov-fail-under=$FAIL_THRESH \
