2e54c5de23aa7517ca73c521dffc2bc323d3c708
Gabe Goodhart <ghart@us.ibm.com>
2021-11-29 15:51:31 -0700

    import_tests: Fix regex so that it works when running with coverage
    Reeeeeaaaallly not sure what's going on here, but :shrug:

--- src/import_tracker/import_tracker.py
+++ src/import_tracker/import_tracker.py
@@ -43,7 +43,7 @@ _module_to_pkg = None
 
 # Exprs for finding module names
 _pkg_version_expr = re.compile("-[0-9]")
-_pkg_name_expr = re.compile("^Name: ([^\s]+)")
+_pkg_name_expr = re.compile("^Name: ([^ \t\n]+)")
 
 # Global mapping of filenames for static tracking files for individual modules
 _static_trackers = {}
