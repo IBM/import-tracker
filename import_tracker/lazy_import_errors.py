e2c5e254a306ac406131788f8112ca4186259401
Alex-Brooks <Alex.Brooks@ibm.com>
2021-11-29 12:43:46 -0700

    Use instances of lazy metafinder on syspath

--- import_tracker/lazy_import_errors.py
+++ import_tracker/lazy_import_errors.py
@@ -20,7 +20,7 @@ def lazy_import_errors():
     standard import semantics
     """
     try:
-        sys.meta_path.append(_LazyMetaFinder)
+        sys.meta_path.append(_LazyMetaFinder())
         yield
     finally:
         sys.meta_path.pop()
@@ -47,42 +47,46 @@ class _LazyMetaFinder(importlib.abc.MetaPathFinder):
     potentially raise an ImportError when the module is used
     """
 
-    @staticmethod
-    def find_spec(fullname, path, *args, **kwargs):
+    def __init__(self):
+        self.calling_pkg = None
+        self.this_module = sys.modules[__name__].__package__.split(".")[0]
+        non_importlib_mods = self._get_non_import_modules()
+        for pkgname in non_importlib_mods:
+            # If this is the first non-initial hit that does match this module
+            # then the previous module is the one calling import_module
+            if self.calling_pkg is None and pkgname not in [self.this_module, 'contextlib']:
+                self.calling_pkg = pkgname
+        print('Initializer non import lib mods: {}'.format(non_importlib_mods))
+        assert self.calling_pkg is not None
+
+
+    def find_spec(self, fullname, path, *args, **kwargs):
         """Since this meta finder is the last priority, it will only be used for
         modules that are not otherwise found. As such, we use it to set up a
         lazy ModuleNotFoundError that will trigger when the module is used
         rather than when it is imported.
         """
-
-        # Figure out the module that is doing the import and the module that is
-        # calling import_module
-        stack = inspect.stack()
-        this_module = sys.modules[__name__].__package__.split(".")[0]
         importing_pkg = None
-        calling_pkg = None
-        non_importlib_mods = list(filter(lambda x: x != "importlib", [
-            frame.frame.f_globals["__name__"].split(".")[0]
-            for frame in stack
-        ]))
-        for i, pkgname in enumerate(non_importlib_mods):
-            if pkgname == "importlib":
-                continue
+        non_importlib_mods = self._get_non_import_modules()
 
+        for i, pkgname in enumerate(non_importlib_mods):
             # If this is the first hit beyond this module, it's the module doing
             # the import
-            if importing_pkg is None and pkgname != this_module:
+            if importing_pkg is None and pkgname != self.this_module:
                 importing_pkg = pkgname
 
-            # If this is the first non-initial hit that does match this module
-            # then the previous module is the one calling import_module
-            if i > 0 and calling_pkg is None and pkgname == this_module:
-                calling_pkg = non_importlib_mods[i-1]
+        # DEBUG
+        # print('Non importlib mods')
+        # print(non_importlib_mods)
+        # print('Importing package: {}'.format(importing_pkg))
+        # print('Calling package: {}'.format(self.calling_pkg))
+        # print('This module: {}'.format(self.this_module))
+        # print('Full name: {}\n\n'.format(fullname))
 
-        assert None not in [importing_pkg, calling_pkg], "Could not determine calling and importing pkg"
+        assert None not in [importing_pkg, self.calling_pkg], "Could not determine calling and importing pkg"
 
         # If the two are not the same, don't mask this with lazy errors
-        if importing_pkg != calling_pkg:
+        if importing_pkg != self.calling_pkg:
             return None
 
         # Set up a lazy loader that wraps the Loader that defers the error to
@@ -93,3 +97,15 @@ class _LazyMetaFinder(importlib.abc.MetaPathFinder):
         # Create a spec from this loader so that it acts at import-time like it
         # loaded correctly
         return importlib.util.spec_from_loader(fullname, lazy_loader)
+
+    ## Implementation Details ######################################################
+    @staticmethod
+    def _get_non_import_modules():
+        # Figure out the module that is doing the import and the module that is
+        # calling import_module
+        stack = inspect.stack()
+        non_importlib_mods = list(filter(lambda x: x != "importlib", [
+            frame.frame.f_globals["__name__"].split(".")[0]
+            for frame in stack
+        ]))
+        return non_importlib_mods
