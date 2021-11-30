3c9ce0493ca2bc2a5bc4a8943d062f476ba8c495
Gabe Goodhart <ghart@us.ibm.com>
2021-11-30 11:48:01 -0700

    ResetModulesFixture: Use a fixture to reset sys.modules for each test

---
+++ test/helpers.py
@@ -0,0 +1,25 @@
+"""
+Shared test helpers
+"""
+
+# Standard
+import sys
+
+# Third Party
+import pytest
+
+
+@pytest.fixture
+def reset_sys_modules():
+    """This fixture will reset the sys.modules dict to only the keys held before
+    the test initialized
+    """
+    before_keys = list(sys.modules.keys())
+    yield
+    added_keys = [
+        module_name for module_name in sys.modules.keys()
+        if module_name not in before_keys
+    ]
+    for added_key in added_keys:
+        mod = sys.modules.pop(added_key)
+        del mod
--- test/test_import_tracker.py
+++ test/test_import_tracker.py
@@ -1,5 +1,5 @@
 """
-Tests for the lazy_import_errors functionality
+Tests for the import_tracker module's public API
 """
 
 # Standard
@@ -12,30 +12,27 @@ import pytest
 from test.helpers import reset_sys_modules
 import import_tracker
 
-######################## Tests for Direct Invocation of the Context Manager #######################
-def test_lazy_import_sad_package(reset_sys_modules):
+def test_lazy_import_tracker_sad_package(reset_sys_modules):
     """This test makes sure that the ModuleNotFoundError is not raised for an
     unknown module on import, but that it is raised on attribute access.
 
-    This version tests that this is true when imported directly, but wrapped in
-    lazy_import_errors.
+    This version tests that this is true when imported via
+    import_tracker.import_module
     """
-    with import_tracker.lazy_import_errors():
-        import foobarbaz
+    foobarbaz = import_tracker.import_module("foobarbaz")
     with pytest.raises(ModuleNotFoundError):
         foobarbaz.foo()
 
-def test_lazy_import_happy_package_with_sad_optionals(reset_sys_modules):
+def test_lazy_import_tracker_happy_package_with_sad_optionals(reset_sys_modules):
     """This test uses `numpy` which has several "optional" dependencies in order
     to support backwards compatibility. We need to ensure that these usecases
     are supported such that the downstream libs do not get confused.
 
-    This version tests that the import works when imported directly, but wrapped
-    in lazy_import_errors.
+    This version tests that the import works when imported via
+    import_tracker.import_module
 
     CITE: https://github.com/numpy/numpy/blob/main/numpy/compat/py3k.py#L24
     """
     import pickle
-    with import_tracker.lazy_import_errors():
-        import numpy
-        assert numpy.compat.py3k.pickle is pickle
+    numpy = import_tracker.import_module("numpy")
+    assert numpy.compat.py3k.pickle is pickle
