00174d17defa462d36c3cf19138c50e4345a2fc2
Gabe Goodhart <ghart@us.ibm.com>
2022-02-15 15:44:42 -0700

    AutoTrackingRewrite: Add level constants to logging

--- import_tracker/log.py
+++ import_tracker/log.py
@@ -9,16 +9,14 @@ import logging
 
 log = logging.getLogger("IMPRT")
 
+# Add higher-order levels as constants to logging
+setattr(logging, "DEBUG1", logging.DEBUG - 1)
+setattr(logging, "DEBUG2", logging.DEBUG - 2)
+setattr(logging, "DEBUG3", logging.DEBUG - 3)
+setattr(logging, "DEBUG4", logging.DEBUG - 4)
+
 # Add higher-order logging
-setattr(
-    log, "debug1", lambda *args, **kwargs: log.log(logging.DEBUG - 1, *args, **kwargs)
-)
-setattr(
-    log, "debug2", lambda *args, **kwargs: log.log(logging.DEBUG - 2, *args, **kwargs)
-)
-setattr(
-    log, "debug3", lambda *args, **kwargs: log.log(logging.DEBUG - 3, *args, **kwargs)
-)
-setattr(
-    log, "debug4", lambda *args, **kwargs: log.log(logging.DEBUG - 4, *args, **kwargs)
-)
+setattr(log, "debug1", lambda *args, **kwargs: log.log(logging.DEBUG1, *args, **kwargs))
+setattr(log, "debug2", lambda *args, **kwargs: log.log(logging.DEBUG2, *args, **kwargs))
+setattr(log, "debug3", lambda *args, **kwargs: log.log(logging.DEBUG3, *args, **kwargs))
+setattr(log, "debug4", lambda *args, **kwargs: log.log(logging.DEBUG4, *args, **kwargs))
