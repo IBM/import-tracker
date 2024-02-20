52318ea03ba8709903580906bd07c14c72d074d9
Gabe Goodhart <ghart@us.ibm.com>
2024-02-20 10:51:56 -0700

    main: Disable buildkit in release build
    The release-test phase needs to run after the release phase!
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,11 @@ cd $(dirname ${BASH_SOURCE[0]})/..
 # Get the tag for this release
 tag=$(echo $REF | cut -d'/' -f3-)
 
+# We explicitly don't want to run with buildkit so that the docker builds happen
+# in a linear fashion since our `release_test` stages intentionally don't
+# inherit from the stages where the publication happens.
+export DOCKER_BUILDKIT=0
+
 # Build the docker phase that will release and then test it
 docker build . \
     --target=release_test \
