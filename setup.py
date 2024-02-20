55de80f04be5816f6fc33058c506378c7041f47c
Gabe Goodhart <ghart@us.ibm.com>
2024-02-20 11:10:58 -0700

    FinishNewVersionSupport: Add 3.11 and 3.12 to package metadata
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords=["import", "importlib", "dependencies"],
     packages=["import_tracker"],
